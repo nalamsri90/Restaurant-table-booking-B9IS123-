ff744ff1d0299ba682155f32c764fd505c9d0868
nalamsri90 <150025618+nalamsri90@users.noreply.github.com>
2024-06-13 21:48:46 +0100

    import the flask and render template

---
+++ app.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
